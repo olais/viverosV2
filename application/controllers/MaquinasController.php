36dcb0c5b027dc9c26e3ca3fc5253753b8f15f52
Mi App en línea <olais_relatividad@hotmail.com>
2016-06-13 18:25:25 -0500

    tabs
    se agregó la funcionalidad tabs

---
+++ application/controllers/MaquinasController.php
@@ -0,0 +1,57 @@
+<?php
+
+class MaquinasController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        
+    }
+
+    public function indexAction()
+    {
+          
+        //$this->_helper->layout->disableLayout();
+        //$this->_helper->viewRenderer->setNoRender();
+
+    }
+    public function altasAction(){
+        
+        //$this->_helper->layout->disableLayout();
+        //$this->_helper->viewRenderer->setNoRender();
+      
+         
+    }
+    
+    public function consultarAction(){
+        
+        $this->_helper->layout->disableLayout();
+        $this->_helper->viewRenderer->setNoRender();
+        $response=new stdClass();
+        $con=new Application_Model_Logica();
+        $datos=  $con->consultarTodos();
+         echo json_encode($datos);
+      
+    }
+
+    public function contactosAction(){
+
+        $this->_helper->layout->disableLayout();
+        $this->_helper->viewRenderer->setNoRender();
+        $response=new stdClass();
+        $con=new Application_Model_Logica();
+        $datos=  $con->consultarContactos();
+         
+
+
+
+
+    }
+
+
+
+    
+   
+    
+
+}
