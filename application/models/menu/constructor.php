4081a1abac6d03fa634994dddeda7b09e3730869
Mi App en línea <olais_relatividad@hotmail.com>
2016-06-14 18:04:12 -0500

    menu principal
    se agregó funcionalidad para el menu principal

--- application/models/menu/constructor.php
+++ application/models/menu/constructor.php
@@ -7,6 +7,8 @@ class Application_Model_Menu_Constructor {
     protected  $catalogos;
     protected  $salir;
     protected  $produccion;
+    protected  $entregas;
+    protected  $usuarios;
     protected  $baseUrl;
 
 
@@ -18,30 +20,77 @@ class Application_Model_Menu_Constructor {
     }
          
     function getinicio(){
-        $this->inicio=" <li>
-                            <a href='".$this->baseUrl."/index/vista' >Inicio</a>
+        $this->inicio=" <li class='active'>
+                            <a href='".$this->baseUrl."/index' >Inicio</a>
                          </li>";
     return $this->inicio;
     }
     
-      function getcatalogos(){
-            $this->catalogos="<li><a href='#'>sqsdd</a></li>"; 
-                                 
-                                
+      function getcatalogos($subpermiso){
+            $this->catalogos.="<li class='dropdown'>
+                                <a class='dropdown-toggle' data-toggle='dropdown' href='#'>Catalogos <span class='caret'></span></a>
+                                 <ul class='dropdown-menu'>";
+                 if($subpermiso==1){
+            $this->catalogos.=" <li><a href='consultas'>Clientes</a></li>";
+                                    }
+                                           
+            $this->catalogos.="<!--li><a href='maquinas'>Maquinas</a></li--></ul>  </li> "; 
         return $this->catalogos;
     }
          
      function getplaneacion(){
-            $this->planeacion="<li></li>"; 
+            $this->planeacion="<li><a hrfe='#'>Planeacion</a></li>"; 
                                  
                                 
         return $this->planeacion;
     }
-    
-  
+    function getproduccion($diseno,$corte,$doblado,$armado,$aulado,$router){
+          $this->produccion.="<li class='dropdown'>
+                                <a class='dropdown-toggle' data-toggle='dropdown' href='#'>Produccion<span class='caret'></span></a>
+                                 <ul class='dropdown-menu'>";
+        if($diseno==1){
+            $this->produccion.=" <li><a href='consultas'>Diseño</a></li>";
+        }
+        if($corte==1){
+            $this->produccion.=" <li><a href='consultas'>Corte</a></li>";
+        }
+        if($doblado==1){
+            $this->produccion.=" <li><a href='consultas'>Doblado</a></li>";
+        }
+        if($armado==1){
+            $this->produccion.=" <li><a href='consultas'>Armado</a></li>";
+        }
+        if($aulado==1){
+            $this->produccion.=" <li><a href='consultas'>Aulado</a></li>";
+        }      
+        if($router==1){
+            $this->produccion.=" <li><a href='consultas'>Router</a></li>"; 
+        }
+                                            
+            $this->produccion.="<!--li><a href='maquinas'>Maquinas</a></li--></ul>  </li> "; 
+        return $this->produccion;
+    }
+    function getentregas(){
+            $this->entregas="<li><a hrfe='#'>Entregas</a></li>"; 
+                                 
+                                
+        return $this->entregas;
+    }
+     function getusuarios(){
+            $this->usuarios="<li><a hrfe='#'>Usuarios</a></li>"; 
+                                 
+                                
+        return $this->usuarios;
+    }
+   function getmonitor(){
+            $this->monitor="<li><a hrfe='#'>Monitor</a></li>"; 
+                                 
+                                
+        return $this->monitor;
+    }
     function  getsalir(){
        $this->salir="<li>
-            <a href='".$this->baseUrl."/index/logout'>Salir</a></li>";
+            <a href='".$this->baseUrl."/login/salir'>Salir</a></li>";
         return $this->salir;
 
     }
